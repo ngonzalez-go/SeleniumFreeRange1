3e3def2603c3e182bf5ed937726c79ac40712b2b
Nicolás González Opazo <ngonzalezopazo@gmail.com>
2025-12-29 23:16:31 -0300

    segundo commit

---
+++ src/test/java/pages/PaginaPrincipal.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class PaginaPrincipal {
+
+     public void navigateToFreeRangeTesters() {
+        // lógica de navegación
+        driver.get("https://www.freerangetesters.com");
+    }
+    
+}
